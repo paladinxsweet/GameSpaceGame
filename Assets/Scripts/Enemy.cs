f735b61dc1c773994326f8816d9a4f0a13f2e514
ibrahimhalilmetin <101739706+ibrahimhalilmetin@users.noreply.github.com>
2022-10-30 15:29:21 +0300

    paladinxsweet/GameSpaceGame
    unityHub

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,82 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField]
+    private float _speed = 4.0f;
+
+
+    private Player _player;
+    private Animator _anim;
+    private AudioSource _audioSource;
+
+    void Start()
+    {
+        _player = GameObject.Find("Player").GetComponent<Player>();
+        _audioSource = GetComponent<AudioSource>();
+        if(_player==null)
+        {
+
+            Debug.LogError("The Player is NULL");
+
+        }
+        _anim = GetComponent<Animator>();
+        if(_anim == null)
+        {
+
+            Debug.LogError("The Animator is NULL");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.down * _speed * Time.deltaTime);
+
+
+        if(transform.position.y < -5f)
+        {
+            float ramdomx = Random.Range(-8f, 8f);
+            transform.position = new Vector3(ramdomx, 7, 0);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.tag == "Player")
+        {
+            Player player = other.transform.GetComponent<Player>();
+            
+            if(player != null)
+            {
+                player.Damage();
+            }
+            _anim.SetTrigger("OnEnemyDeath");
+            _speed = 0;
+            _audioSource.Play();
+            Destroy(this.gameObject,2.8f);
+            
+        }
+
+        if(other.tag == "Laser")
+        {
+            Destroy(other.gameObject);
+           if(_player != null)
+            {
+                _player.AddScore(10);
+
+            }
+            _anim.SetTrigger("OnEnemyDeath");
+            _speed = 0;
+            _audioSource.Play();
+            Destroy(this.gameObject,2.8f);
+            
+        }
+
+    }
+}
+
